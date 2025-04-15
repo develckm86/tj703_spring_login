1a5b402c0ce83ef74f5d5ac7fb716e0563201f4d
develckm86 <develckm86@gmail.com>
2025-04-15 13:13:44 +0900

    로그인 액션에 쿠키추가

--- src/main/java/com/tj703/l09_spring_login/controller/UserController.java
+++ src/main/java/com/tj703/l09_spring_login/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.tj703.l09_spring_login.controller;
 
 import com.tj703.l09_spring_login.entity.User;
+import com.tj703.l09_spring_login.jwt.JwtUtil;
 import com.tj703.l09_spring_login.security.CustomUserDetails;
 import com.tj703.l09_spring_login.security.CustomUserDetailsService;
 import com.tj703.l09_spring_login.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
@@ -25,6 +28,7 @@ public class UserController {
 
     private final UserService userService;
     private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtil jwtUtil;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @GetMapping("/login.do")
@@ -41,7 +45,8 @@ public class UserController {
     // ?id=admin1&pw=1234
     @PostMapping("/jwt/login.do")
     public String loginAction(
-            @ModelAttribute User user
+            @ModelAttribute User user,
+            HttpServletResponse response
             ) {
         System.out.println("loginAction 중!");
         UserDetails userDetails = null;
@@ -57,6 +62,12 @@ public class UserController {
         SecurityContextHolder.getContext().setAuthentication(authToken);
         //시큐리티에서 관리하는 로그인 유저(UserDetails)
 
+        Cookie jwt=new Cookie("jwt",jwtUtil.generateToken(userDetails.getUsername()));
+        jwt.setHttpOnly(true); //js 쿠키탈취 못하게
+        //jwt.setSecure(true); //https 통신에서만 쿠키보내겠다.
+        jwt.setPath("/");
+        jwt.setMaxAge(1000*60*30);
+        response.addCookie(jwt);
         return "redirect:/";
     }
     @GetMapping("/logout.do")
