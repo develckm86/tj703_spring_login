c4560c061730b654ed5231626ac8c1c67cab4617
develckm86 <develckm86@gmail.com>
2025-04-22 17:00:03 +0900

    cors 추가

--- src/main/java/com/tj703/l09_spring_login/controller/AdminController.java
+++ src/main/java/com/tj703/l09_spring_login/controller/AdminController.java
@@ -3,7 +3,9 @@ package com.tj703.l09_spring_login.controller;
 import com.tj703.l09_spring_login.entity.User;
 import com.tj703.l09_spring_login.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,14 @@ import java.util.List;
 @RestController //data를 전달만하는 컨트롤러 : fsr MPA
 @RequestMapping("/admin")
 @AllArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class AdminController {
     private UserService userService;
-    @GetMapping("/user/list.do")
-    public List<User> userList() {
-        return userService.list();
-    }
 
+    @GetMapping("/user/list.do")
+    public ResponseEntity<List<User>> userList() {
+        System.out.printf("admin user list");
+        List<User> userList=userService.list();
+        return ResponseEntity.ok(userList);
+    }
 }
